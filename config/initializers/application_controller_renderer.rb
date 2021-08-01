dd66398fe8801d90c502d9c27a0e1b3ac26fff23
world-start <23fn40@gmail.com>
2021-08-01 09:58:12 +0000

    [Add2]meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '96fd5a47e914741636af487b09c73a5a71c452a821effe4dd38cfc10f53568c87ac5bfc437d57b6b6edfb8697f26c22b149d2a3209534ba0fa166ba491140a64'
